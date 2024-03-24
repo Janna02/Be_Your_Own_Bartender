81a2ee4a14dc9a70f90d7bab4428548b76aaf1d5
Жанна Титова <dendy2@yandex.ru>
2024-03-24 07:15:42 +0300

    Final_test

---
+++ cookbook/src/components/app/SideBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{ open: modelValue }">
+    <!-- Используем router-link для навигации по ссылкам -->
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      :to="link.url"
+      active-class="active"
+    >
+      <li>
+        <!-- Ссылка в боковой панели с названием ссылки -->
+        <a class="waves-effect waves-purple pointer">{{ link.title }}</a>
+      </li>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  // Принимаем значение модели в качестве prop
+  props: {
+    modelValue: Boolean,
+  },
+  // Инициализируем данные компонента
+  data: () => ({
+    // Список ссылок для боковой панели
+    links: [
+      { title: "Home", url: "/" },
+      { title: "Create Recipe", url: "/recipecreate" },
+      { title: "Profile", url: "/profile" },
+    ],
+  }),
+};
+</script>
